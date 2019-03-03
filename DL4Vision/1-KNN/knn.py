cc26058afc1f7eb64fb5143c3d618ffb37259259
yinyunjie <yunjieyin@163.com>
2019-03-03 09:45:02 +0800

    DL4Vision knn

---
+++ DL4Vision/1-KNN/knn.py
@@ -0,0 +1,42 @@
+"""
+KNN classifier for animal dataset: dog, cat
+
+#USAGE: python knn.py --dataset /media/yinyunjie/WD_HDD/dataSets/dogs-cats/train --neighbors 2
+"""
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+
+from functional_module import SimpleDatasetLoader, SimplePreprocessor
+from imutils import paths
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--dataset", required=True, help="path to input dataset")
+ap.add_argument("-k", "--neighbors", type=int, default=1, help="number of k for classification")
+ap.add_argument("-j", "--jobs", type=int, default=-1, help="# of jobs for run")
+args = vars(ap.parse_args())
+
+print("[INFO] loading images...")
+imagePaths = list(paths.list_images(args["dataset"]))
+
+sp = SimplePreprocessor(32, 32)
+sdl = SimpleDatasetLoader(preprocessors=[sp])
+(data, labels) = sdl.load(imagePaths, verbose=500)
+data = data.reshape((data.shape[0], 3072))
+
+print("[INFO] features matrix: {:.1f}MB".format(data.nbytes / (1024 * 1000.0)))
+
+le = LabelEncoder()
+labels = le.fit_transform(labels)
+
+(trainX, testX, trainY, testY) = train_test_split(data, labels, test_size=0.25, random_state=42)
+
+print("[INFO] evaluating k-NN classifier...")
+model = KNeighborsClassifier(n_neighbors=args["neighbors"], n_jobs=args["jobs"])
+model.fit(trainX, trainY)
+
+# Classification report
+print(classification_report(testY, model.predict(testX), target_names=le.classes_))
